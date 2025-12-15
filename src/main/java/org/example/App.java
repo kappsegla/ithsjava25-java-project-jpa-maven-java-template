226730086a2d3f02e574bd841265b0278c3659e5
Martin Blomberg <martin.blomberg@outlook.com>
2025-12-15 10:28:31 +0100

    Add utility method for addition in App class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,4 +4,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int add(int a, int b) {
+        int[] intArray = {a,b};
+        int sum = intArray[0] + intArray[1];
+        return sum;
+    }
 }
